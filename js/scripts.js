435c8696357b9dfb66b65b2f33ec5c94a3c9041e
Fernando Baladei <f.baladei@gmail.com>
2023-10-06 06:55:34 -0300

    done password generator

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,60 @@
-console.log("teste");
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
+
+function getLetterLowerCase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getLetterUpperCase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getNumber() {
+  return Math.floor(Math.random() * 10).toString();
+}
+
+function getSymbol() {
+  const symbols = "!@#$%&*.,()_+=-[]{}^~/<>";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+function generatePassword(
+  getLetterLowerCase,
+  getLetterUpperCase,
+  getNumber,
+  getSymbol
+) {
+  let password = "";
+
+  const passwordLenght = 10;
+
+  const generators = [
+    getLetterLowerCase,
+    getLetterUpperCase,
+    getNumber,
+    getSymbol,
+  ];
+
+  for (i = 0; i < passwordLenght; i = i + generators.length) {
+    generators.forEach(() => {
+      const randomValue =
+        generators[Math.floor(Math.random() * generators.length)]();
+
+      password += randomValue;
+    });
+  }
+
+  password = password.slice(0, passwordLenght);
+
+  generatedPasswordElement.style.display = "block";
+  generatedPasswordElement.querySelector("h4").innerText = password;
+}
+
+generatePasswordButton.addEventListener("click", () => {
+  generatePassword(
+    getLetterLowerCase,
+    getLetterUpperCase,
+    getNumber,
+    getSymbol
+  );
+});
